883d43b811fabf35d27041a9e6a84aaf2cd05877
unknown <aannddrree>
2021-03-16 21:10:34 -0300

    Introdução

---
+++ ProjDesignPattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using AbstractFactory;
+using Facade;
+using Observer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //new ClientAbstractFactory().Main(); //AbstractFactory
+            //new ClientFacade().Main();          //Facade
+            new ClientObserver().Main();          //Observer
+            
+            Console.ReadKey();
+        }
+    }
+}
